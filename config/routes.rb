90d9a842ad9de22f8218ddd512ac819759d737f1
dai954 <da0606ha@gmail.com>
2020-01-22 14:47:14 +0900

    adminPage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'places#index'
   namespace :places do
     resources :searches, only: :index
   end
-  resources :places, only: [:index, :show, :new]
+  resources :places, only: [:index, :show, :new] do
+  collection do
+    get 'searchword'
+  end
+end
   resources :posts
   resources :users, only: :show
 end
